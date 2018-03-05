2ec517fcc412827504aff0a4cb68084a49a01dbc
Dominic Torruellas <dtorruellas@gmail.com>
2018-03-04 19:45:23 -0700

    types

---
+++ 02-Typescript-Fundamentals/03-Types/types/main.js
@@ -0,0 +1,14 @@
+var a;
+var b;
+var c;
+var d;
+var e;
+var f;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Red;
---
+++ 02-Typescript-Fundamentals/03-Types/types/main.ts
@@ -0,0 +1,10 @@
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[];
+let f: any[];
+
+enum Color { Red = 0, Green = 1, Blue = 2 };
+
+let backgroundColor = Color.Red;
