444102fd21e08c0e8cf36f23064b7997bd0b7d55
Dominic Torruellas <dtorruellas@gmail.com>
2018-03-10 17:59:32 -0700

    ngFor

---
+++ 06-Directives/01-Section-Work/03-ngFor/ng-for/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  courses: object[];
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4 ' });
+  }
+
+  onRemove(course) {
+    const index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'course1 ' },
+      { id: 2, name: 'course2 ' },
+      { id: 3, name: 'course3 ' },
+    ];
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
+  }
+}
